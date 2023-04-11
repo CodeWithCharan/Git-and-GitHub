18dfb9fd2f0e446c573532ba03443b4f5766164c
Charan Thoutam <thoutamsricharan@gmail.com>
2023-04-11 22:02:45 +0530

    Updated learn.py

--- learn.py
+++ learn.py
@@ -1,2 +1,3 @@
 print("learn Git")
 print("Git is a version control system that allows you to track changes in your code and collaborate with others on a project.")
+print("Here are some of the essential concepts you should learn in Git: Repository, Commit, Branches, Merge, Pull Request, Git Workflow")
